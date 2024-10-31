2aa6ba5de10c939097a7d03399266164e03fad5d
Joao-ava <ajoao781238@gmail.com>
2024-10-31 09:05:15 -0300

    Adicionado classe base da matriz com testes unitarios

---
+++ app/matrix.py
@@ -0,0 +1,44 @@
+class Matrix:
+    def __init__(self, linha, coluna, matrizelementos):
+        self.rows = linha
+        self.cols = coluna
+        self.matrix = []
+        for i in range(linha):
+            linha_da_matriz = []
+            for j in range(coluna):
+                elemento = matrizelementos[i * coluna + j]
+                linha_da_matriz.append(elemento)
+
+            self.matrix.append(linha_da_matriz)
+
+    def get(self, i, j):
+        return self.matrix[i - 1][j - 1]
+
+    def set(self, i, j, value):
+        if i < 1 or i > self.rows:
+            raise Exception('Essa matriz não possui a linha ' + str(i))
+        if j < 1 or j > self.cols:
+            raise Exception('Essa matriz não possui a coluna ' + str(j))
+
+        self.matrix[i-1][j-1] = value
+
+    @property
+    def elements(self):
+        elements = []
+        for row in self.matrix:
+            for item in row:
+                elements.append(item)
+        return elements
+
+    def __str__(self):
+        max_elements_size = max([len(str(item)) for item in self.elements])
+        content = ''
+        content += ('-' * max_elements_size * self.cols) + '---' * self.cols
+        content += '\n'
+        for row in self.matrix:
+            content += '|'
+            content += ' | '.join([f'{item:<{max_elements_size}}' for item in row])
+            content += '|\n'
+
+        content += ('-' * max_elements_size * self.cols) + '---' * self.cols
+        return content
---
+++ tests.py
@@ -0,0 +1,60 @@
+import unittest
+from app.matrix import Matrix
+
+
+class TestMatrix(unittest.TestCase):
+    def test_create(self):
+        elements = [
+            1, 0, 0,
+            2, 1, 0,
+            3, 2, 1,
+            4, 3, 2
+        ]
+        matrix = Matrix(4, 3, elements)
+        self.assertEqual(matrix.rows, 4)
+        self.assertEqual(matrix.cols, 3)
+        self.assertSequenceEqual(matrix.elements, elements)
+
+    def test_get(self):
+        elements = [
+            4, 5, 6,
+            2, 1, 0,
+            3, 2, 1
+        ]
+        matrix = Matrix(3, 3, elements)
+        self.assertEqual(matrix.get(1, 1), 4)
+        self.assertEqual(matrix.get(1, 2), 5)
+        self.assertEqual(matrix.get(1, 3), 6)
+
+        self.assertEqual(matrix.get(2, 1), 2)
+        self.assertEqual(matrix.get(2, 2), 1)
+        self.assertEqual(matrix.get(2, 3), 0)
+
+        self.assertEqual(matrix.get(3, 1), 3)
+        self.assertEqual(matrix.get(3, 2), 2)
+        self.assertEqual(matrix.get(3, 3), 1)
+
+        
+        elements = [
+            4, 5, 6,
+            3, 2, 1
+        ]
+        matrix = Matrix(2, 3, elements)
+        self.assertEqual(matrix.get(1, 1), 4)
+        self.assertEqual(matrix.get(1, 2), 5)
+        self.assertEqual(matrix.get(1, 3), 6)
+
+        self.assertEqual(matrix.get(2, 1), 3)
+        self.assertEqual(matrix.get(2, 2), 2)
+        self.assertEqual(matrix.get(2, 3), 1)
+
+    def test_set(self):
+        elements = [
+            1, 0, 0,
+            2, 1, 0,
+            3, 2, 1
+        ]
+        matrix = Matrix(3, 3, elements)
+        self.assertEqual(matrix.get(1, 1), 1)
+        matrix.set(1, 1, 2)
+        self.assertEqual(matrix.get(1, 1), 2)
